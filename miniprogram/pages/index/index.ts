2b9bea88bc5ae5d8698b73286cd0bdbee90cddac
unknown <unknown@example.com>
2023-04-02 11:28:20 +0800

    Initial Commit

---
+++ miniprogram/pages/index/index.ts
@@ -0,0 +1,73 @@
+// index.ts
+// 获取应用实例
+const app = getApp<IAppOption>()
+
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {},
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo'),
+    canIUseGetUserProfile: false,
+    canIUseOpenData: wx.canIUse('open-data.type.userAvatarUrl') && wx.canIUse('open-data.type.userNickName'), // 如需尝试获取用户信息可改为false
+    countdown: '00:00:00',
+    targetDate: new Date('2022-11-20 19:59:59'), // 设置目标日期，例如：new Date('2023-12-31 23:59:59')
+  },
+  // 事件处理函数
+  bindViewTap() {
+    wx.navigateTo({
+      url: '../logs/logs',
+    })
+  },
+  onLoad() {
+    // @ts-ignore
+    if (wx.getUserProfile) {
+      this.setData({
+        canIUseGetUserProfile: true
+      })
+    }
+
+    // 开始倒计时
+    this.startCountdown();
+  },
+  startCountdown() {
+    const updateCountdown = () => {
+      const currentTime = new Date();
+      // const remainingTime = this.data.targetDate - currentTime; // debuged by chatGPT
+      const remainingTime = (this.data.targetDate as any as number) - (currentTime as any as number);
+      
+      const hours = Math.floor(remainingTime / (1000 * 60 * 60));
+      const minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+
+      this.setData({
+        countdown: `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`
+      });
+
+      setTimeout(updateCountdown, 1000);
+    }
+
+    updateCountdown();
+  },
+  getUserProfile() {
+    // 推荐使用wx.getUserProfile获取用户信息，开发者每次通过该接口获取用户个人信息均需用户确认，开发者妥善保管用户快速填写的头像昵称，避免重复弹窗
+    wx.getUserProfile({
+      desc: '展示用户信息', // 声明获取用户个人信息后的用途，后续会展示在弹窗中，请谨慎填写
+      success: (res) => {
+        console.log(res)
+        this.setData({
+          userInfo: res.userInfo,
+          hasUserInfo: true
+        })
+      }
+    })
+  },
+  getUserInfo(e: any) {
+    // 不推荐使用getUserInfo获取用户信息，预计自2021年4月13日起，getUserInfo将不再弹出弹窗，并直接返回匿名的用户个人信息
+    console.log(e)
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  }
+})
